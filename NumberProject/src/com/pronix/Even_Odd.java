8b3826acf1c21dbd3569a938979a3783a0565f39
PRONIX-DEV-57A <PRONIX-DEV-57A@PRONIX-DEV>
2024-11-13 15:09:07 +0530

    2nd Commit

---
+++ NumberProject/src/com/pronix/Even_Odd.java
@@ -0,0 +1,15 @@
+package com.pronix;
+
+import java.util.Scanner;
+
+public class Even_Odd {
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int num=sc.nextInt();
+		if(num%2==0) {
+			System.out.println("Even Number...");
+		}
+		else System.out.println("Odd Number...");
+	}
+}
